2d3099bb2064112fa38ad906e94ec8f3c1750c23
John Russell <jruss600@gmail.com>
2019-10-30 20:46:19 -0600

    Updates

---
+++ precache-manifest.acc8407740bd3c54a2be41b54b3606f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797bec48b25915bb00223e878ecefa3c",
+    "url": "/nfl-pool/index.html"
+  },
+  {
+    "revision": "87a8e14b6b773bf26154",
+    "url": "/nfl-pool/static/css/main.b2232873.chunk.css"
+  },
+  {
+    "revision": "5521f77d79f03dc9dba0",
+    "url": "/nfl-pool/static/js/2.895dfb5a.chunk.js"
+  },
+  {
+    "revision": "87a8e14b6b773bf26154",
+    "url": "/nfl-pool/static/js/main.b8a4ca57.chunk.js"
+  },
+  {
+    "revision": "faa1126bdc46ebc684e2",
+    "url": "/nfl-pool/static/js/runtime-main.88684161.js"
+  }
+]);
